2ac79f6658eee19a956ab7435ac3793ccd739037
Gesss26 <gesualdo.sangiorgi@gmail.com>
2026-09-02 08:56:35 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 # CONFIGURAZIONE
 # ============================================================
 
-TOKEN = "7674593142:AAGhP_A5x9XIHQ1BKKufDA0jwjn2k2KerJg"
+TOKEN = "7674593142:AAFL7VQMDjnBn2ZONONzis4iUN6gKKCQOJ4"
 EXCEL_URL = "https://raw.githubusercontent.com/Gesss26/GesssAI-Pro---Auto/master/excel/GesssAI_Input.xlsx"
 SPLASH_URL = "https://raw.githubusercontent.com/Gesss26/GesssAI-Pro---Auto/master/Splashscreen.png"
 
